e5b8485870b09aa54d5c1403f2e5a41f578aa2a3
Jared <shmup@dev.host>
2016-07-20 03:00:04 -0400

    backing up

---
+++ cbro_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# usage: source <(curl -s http://dev.host/~shmup/crawl/cbro_install.sh)
+
+mkdir -p $HOME/.ssh && cd $HOME/.ssh
+curl -O http://crawl.berotato.org/crawl/keys/cbro_key
+chmod 400 cbro_key
+echo alias crawl=\"ssh -i $HOME/.ssh/cbro_key crawler@crawl.berotato.org\" >> $HOME/.bashrc
+alias crawl="ssh -i $HOME/.ssh/cbro_key crawler@crawl.berotato.org"
+echo "done. type crawl to play."
